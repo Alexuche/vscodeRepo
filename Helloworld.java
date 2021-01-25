24b08a0d83a0160d291dcf9e5c615cecfb1f465b
Alex Uche <nnayidb@outlook.com>
2021-01-25 04:46:26 +0100

    first commit

--- Helloworld.java
+++ Helloworld.java
@@ -4,6 +4,6 @@
 public class Helloworld {
 
     public static void main(String[] args) {
-        System.out.println("Hello world please help");
+        System.out.println("Hello world please help now");
     }
 }
